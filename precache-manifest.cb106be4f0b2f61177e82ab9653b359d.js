699c744683b30b7e289ade9d019ba68f100a87e8
Duong Dat <tao@cu8-3.local>
2019-12-31 04:28:07 +0700

    first build

---
+++ precache-manifest.cb106be4f0b2f61177e82ab9653b359d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c7a8938bd9be495e3bd54945b535ec",
+    "url": "/flip-card-demo/index.html"
+  },
+  {
+    "revision": "7d522086017c74007a5d",
+    "url": "/flip-card-demo/static/css/main.01d54325.chunk.css"
+  },
+  {
+    "revision": "4daae22f0b56d6f9e476",
+    "url": "/flip-card-demo/static/js/2.66701584.chunk.js"
+  },
+  {
+    "revision": "7d522086017c74007a5d",
+    "url": "/flip-card-demo/static/js/main.72881923.chunk.js"
+  },
+  {
+    "revision": "a5d9ea6832d7a139d7dd",
+    "url": "/flip-card-demo/static/js/runtime-main.0cd45e5c.js"
+  }
+]);
